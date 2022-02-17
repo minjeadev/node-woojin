95ef987edf189c520dab34f12203681685be273e
minjeadev <93039870+minjeadev@users.noreply.github.com>
2022-02-17 16:25:13 +0900

    Create error.ts

---
+++ src/type/error.ts
@@ -0,0 +1,23 @@
+export class WoojinlangError {
+  static Error = (message:string) => {
+    let err = new Error(message);
+    err.name = 'WoojinError';
+    return err
+  };
+  static UNEX_TOKEN = (token:string) => {
+    let err = new Error(`예기치 않은 토큰: ${token}`);
+    err.name = 'SyntaxError';
+    return err
+  };
+  static SYNTAX_ERROR = (message:string) => {
+    let err = new Error(message);
+    err.name = 'SyntaxError';
+    return err
+  };
+
+  static TYPE_ERROR = (message:string) => {
+    let err = new Error(message);
+    err.name = 'TypeError';
+    return err
+  };
+}
